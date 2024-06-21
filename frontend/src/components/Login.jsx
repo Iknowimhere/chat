8221cbf8264f14757b6670089dca06b99c7e899e
umashankar <umashankarp33@gmail.com>
2024-06-21 12:07:59 +0530

    connect frontend auth with backend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,40 @@
-
+import { useState } from "react"
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
+  const navigate=useNavigate()
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+
+  const handleSubmit=async (e)=>{
+    e.preventDefault()
+
+    let formData=new FormData()
+    formData.append("email",email)
+    formData.append("password",password)
+    
+    let {data} = await axios.post(
+      "http://localhost:5000/api/v1/user/login",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    localStorage.setItem("token",data.token)
+    navigate("/chats",{replace:true})
+  }
   return (
     <div className='login-form'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Login</h1>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="name" />
+            <input type="email" name="email" id="name" onChange={(e)=>setEmail(e.target.value)} value={email}/>
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
             <div className="buttons">
             <button type="submit">Login</button>
-            <button type="reset">Clear</button>
             </div>
         </form>
     </div>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,37 +1,81 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Signup = () => {
-const [name,setName]=useState("");
-const [email,setEmail]=useState("");
-const [password,setPassword]=useState("");
-const [confirmPassword,setConfirmPassword]=useState("");
-const [pic,setPic]=useState("")
+  const navigate=useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pic, setPic] = useState("");
 
-const handleSubmit=(e)=>{
-    e.preventDefault()
-    let formData=new FormData()
-    formData.append("name",name)
-    formData.append("email",email)
-    formData.append("password",password)
-    formData.append("confirmPassword",confirmPassword)
-    formData.append("photo",pic)
-    console.log(formData);  
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(name, email, password, confirmPassword, pic);
+    let formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("confirmPassword", confirmPassword);
+    formData.append("photo", pic);
+
+    let {data} = await axios.post(
+      "http://localhost:5000/api/v1/user/register",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    localStorage.setItem("token",data.token)
+    navigate("/chats",{replace:true})
+  };
   return (
     <div className="signup-form">
-        <h1>Sign up</h1>
-      <form onSubmit={handleSubmit}>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="name" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <input
+          type="email"
+          name="email"
+          id="name"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <label htmlFor="confirmPassword">Confirm Password</label>
-        <input type="password" name="confirmPassword" id="confirmPassword" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <label htmlFor="photo">Photo</label>
-        <input type="file" name="photo" id="photo" onChange={(e)=>setPic(e.target.files[0])}/>
-        <div className="btn-container">
+        <input
+          type="file"
+          name="photo"
+          id="photo"
+          onChange={(e) => setPic(e.target.files[0])}
+        />
+        <div className="buttons">
           <button type="submit">Register</button>
           <button type="reset">Clear</button>
         </div>
