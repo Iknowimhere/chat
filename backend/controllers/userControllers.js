f9ec31d2bfffcc7dfeb8ed47e2ba2aaa27bc4eee
umashankar <umashankarp33@gmail.com>
2024-06-25 11:59:30 +0530

    add auth middleware and find users feature

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,45 +1,67 @@
-import User from "../models/User.js"
-import asyncHandler from "express-async-handler"
-import { genToken } from "../utils/generateToken.js"
-
+import User from "../models/User.js";
+import asyncHandler from "express-async-handler";
+import { genToken } from "../utils/generateToken.js";
 
 //register
-export const register=asyncHandler(async (req,res)=>{
-    const {name,email,password,confirmPassword}=req.body
-    const existingUser=await User.findOne({email})
-    console.log(name,email,password,confirmPassword);
-    if(existingUser){
-        throw new Error("User exists already")
-    }
-    const newUser= await User.create({
-        name,email,password,confirmPassword,photo:req.file.path
-    })
-    let token=await genToken(newUser._id)
-    res.status(201).json({
-        status:"Success",
-        data:newUser,
-        token
-    })
-})
-
-
+export const register = asyncHandler(async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  const existingUser = await User.findOne({ email });
+  console.log(name, email, password, confirmPassword);
+  if (existingUser) {
+    throw new Error("User exists already");
+  }
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    confirmPassword,
+    photo: req.file.path,
+  });
+  let token = await genToken(newUser._id);
+  res.status(201).json({
+    status: "Success",
+    data: newUser,
+    token,
+  });
+});
 
 //login
-export const login=asyncHandler(async (req,res,next)=>{
-    const {email,password}=req.body
-    const existingUser=
-    await User.findOne({email})
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
 
+  if (
+    !existingUser ||
+    !(await existingUser.comparePassword(password, existingUser.password))
+  ) {
+    let err = new Error("No user found,Please Register");
+    next(err);
+  }
+  let token = await genToken(existingUser._id);
+  res.status(200).json({
+    status: "Success",
+    data: existingUser,
+    token,
+  });
+});
 
-
-    if(!existingUser || !(await existingUser.comparePassword(password,existingUser.password))){
-        let err= new Error("No user found,Please Register")
-        next(err)
+// /home?search=akash
+// {search:'akash'}
+export const searchUsers = asyncHandler(async (req, res, next) => {
+    let userId = req.userId;
+    let keyword = req.query.search
+      ? {
+          or$: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
+    let users = await User.find(keyword).find({ _id: { $ne: userId } }).exec();
+    if (!users) {
+      let err = new Error("Users not found");
+      next(err);
     }
-    let token=await genToken(existingUser._id)
-    res.status(200).json({
-        status:"Success",
-        data:existingUser,
-        token
-    })
-})
+    res.status(200).json(users);
+
+});
